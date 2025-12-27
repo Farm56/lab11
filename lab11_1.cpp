6b85aac9840b2cb76969fe9c6ca140f06c91f5df
Farm56 <sstg3455@gmail.com>
2025-12-27 16:32:48 +0700

    complete_lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int totalGrades = sizeof(grades) / sizeof(grades[0]);
+
+    cout << "Press Enter 3 times to reveal your future.";
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    srand(time(0));
+    int randomIndex = rand() % totalGrades;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102.";
+
+    return 0;
+}
+
+
